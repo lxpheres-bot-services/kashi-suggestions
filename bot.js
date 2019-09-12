7d1b93533293e9b2dbef8619f3a7c51f7c758d06
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2019-09-12 15:25:49 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,24 @@ const client = new Discord.Client();
 const bot = client;
 const fs = require("fs");
 module.exports.run = async () => {
-	const client = new Discord.Client({ disableEveryone: true, fetchAllMembers: true });
+	const client = new Discord.Client({ disableEveryone: true, fetchAllMembers: true });	
+
+	client.commands = new Discord.Collection();
+	
+	fs.readdir("./commands", (err, files) => {
+		if (err) console.log(err);
+		let jsfile = files.filter((f) => f.split(".").pop() === "js");
+		if (jsfile.length <= 0) {
+			console.log("Couldn't find any commands!");
+			return;
+		}
+
+		jsfile.forEach((f) => {
+			let props = require(`./commands/${f}`);
+			console.log(`${f} has been loaded!`);
+			client.commands.set(props.help.name, props);
+		});
+	});
 	client.on("ready", async () => {
 		client.user.setPresence({ game: { type: 3, name: "Kashi Suggestions" }, status: "online" });
 		console.log(`${client.user.tag} has started!`);
