99a81eeccec8a4a44573e5641fbd2d3e2f56d341
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2019-09-12 16:16:31 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,3 +3,11 @@ module.exports.run = async (bot, message) => {
 	const m = await message.channel.send("Pinging...");
 	m.edit(`Pong! Latency is **${m.createdTimestamp - message.createdTimestamp}ms**! API Latency is **${Math.round(bot.ping)}ms**!`);
 };
+module.exports.help = {
+	name: "help",
+	usage: "help [command]",
+	description: "sends all server commands",
+	longdes: "Sends a list of all the command to the user who ran the command. This will show details about a command if said.",
+	mentionedperm: "none",
+	category: "Utility"
+};
